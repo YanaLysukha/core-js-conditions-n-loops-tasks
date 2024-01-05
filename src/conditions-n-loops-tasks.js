cd416ddc80086231ef4ddf0ea8d090aba3c7e5bf
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-05 20:09:32 +0100

    feat: implement is palindrome function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -189,8 +189,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reversedStr = '';
+  for (let i = 0; i < str.length; i += 1) {
+    reversedStr = `${str[i]}${reversedStr}`;
+  }
+  return str === reversedStr;
 }
 
 /**
